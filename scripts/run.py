0086da9f88c25974104cb397be50e1e741553e32
Shiling42 <liangshiling42@gmail.com>
2020-10-26 00:07:06 +0100

    fix run.py

--- scripts/run.py
+++ scripts/run.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from Hyperparameter_optimization import *
 from proj1_helpers import *
 from preprocess_data import *
-
+from Degree_selection import *
 ## Load the training set
 print('- Loading data from training.csv')
 DATA_TRAIN_PATH = '../data/train.csv' # TODO: download train data and supply path here 
@@ -42,9 +42,9 @@ for idx_data,data in enumerate(data_pro):
     #print('│               Group: %i               │'%idx_data)
     #print('└'+'─' * 38+'┘')
     tx = data[0]
-    tx = np.hstack((build_poly(tx, degrees[idx_data]),np.sin(tx)))
-    y = data[1]. reshape(-1,1)
-    initial_w_ = np.zeros((tx.shape[1],1))
+    tx = np.hstack((build_poly(tx, degrees[idx_data]),1/(abs(tx)+1e-10),np.sin(tx)))
+    y = data[1]
+    initial_w_ = np.zeros(tx.shape[1])
     loss_temp, w_temp = ridge_regression(y, tx,lambda_[idx_data])
     w_.append(w_temp)
 #print('\n * Overall Accuracy = %.4f'%accuracy_all)
@@ -59,7 +59,7 @@ data_size = sum([len(data_test[:,1][i]) for i in range(6)])
 y_predict = np.zeros(data_size)
 for idx_data,data in enumerate(data_test):
     tx = data[0]
-    tx = np.hstack((build_poly(tx, degrees[idx_data]),np.sin(tx)))
+    tx = np.hstack((build_poly(tx, degrees[idx_data]),1/(abs(tx)+1e-10),np.sin(tx)))
     label_.append(predict_labels(w_[idx_data], tx,model='ridge_regression'))
     y_predict[data[1]] = predict_labels(w_[idx_data], tx,model='ridge_regression')
 
